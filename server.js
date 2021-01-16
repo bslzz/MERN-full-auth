53b535e4854fe11ba7d3c59980f9d5db2aa0b67b
bslzz <bishalkoid@gmail.com>
2021-01-16 16:10:55 +0200

    server file ready to test route

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+app.use('/', (req, res) => {
+  res.json({ msg: 'Hello' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at ${PORT}`));
