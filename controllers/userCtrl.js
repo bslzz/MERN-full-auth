abccc42148e3b952c93b8a57a3a775353155b248
bslzz <bishalkoid@gmail.com>
2021-01-17 16:33:04 +0200

    accesstoken after login generated

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -94,6 +94,22 @@ module.exports = {
       res.status(500).json({ msg: err.message });
     }
   },
+
+  getAccessToken: (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshtoken;
+      if (!rf_token) return res.status(400).json({ msg: 'Please login' });
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(400).json({ msg: 'Please login now' });
+
+        const access_token = jwtCtrl.createAccessToken({ id: user.id });
+
+        res.json({ access_token });
+      });
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 //email validation
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,11 @@ const router = express.Router();
 const userCtrl = require('../controllers/userCtrl');
 
 router.route('/register').post(userCtrl.register);
+
 router.route('/activation').post(userCtrl.activateEmail);
+
 router.route('/login').post(userCtrl.login);
+
+router.route('/refresh_token').post(userCtrl.getAccessToken);
 
 module.exports = router;
