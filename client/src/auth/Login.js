8207cb52877f1c74c4c999dd4b3f100e222c0e16
bslzz <bishalkoid@gmail.com>
2021-01-20 20:38:20 +0200

    login form created

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>login</div>;
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    err: '',
+    success: '',
+  });
+  const { email, password, err, success } = user;
+  return (
+    <div className="login_page">
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="text"
+            placeholder="Enter your email"
+            id="email"
+            value={email}
+            name="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            id="password"
+            value={password}
+            name="password"
+          />
+        </div>
+        <div className="row">
+          <button type="submit">Login</button>
+          <Link to="/forgot_password">Forgot your password?</Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
